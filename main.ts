97dc62e9daca682039584d1b383e4fa296d8ee21
Leerling Oostpoort <129111356+leerling-oostpoort@users.noreply.github.com>
2023-11-20 21:51:28 +0100

    Implementeer lijnvolger

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_unline)) {
+        cuteBot.motors(25, 25)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(25, 10)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(10, 25)
+    } else {
+        cuteBot.motors(20, 20)
+    }
+})
